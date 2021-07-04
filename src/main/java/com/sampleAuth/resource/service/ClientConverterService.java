f24e71ce0b320904b87f0f254d810b8b3438fc58
Don Lacdan <litlolapidisreal@gmail.com>
2021-07-05 02:11:18 +0800

    Initial Changes

--- src/main/java/com/sampleAuth/resource/service/ClientConverterService.java
+++
@@ -1,9 +0,0 @@
-package com.sampleAuth.resource.service;
-
-import com.sampleAuth.resource.models.Clients;
-import com.sampleAuth.resource.models.CustomClient;
-
-public interface ClientConverterService {
-    Clients convertFromCustom2Client(CustomClient client);
-    Boolean convertFromClient2Custom(Clients client);
-}
--- src/main/java/com/sampleAuth/resource/service/RegistrationService.java
+++
@@ -1,9 +0,0 @@
-package com.sampleAuth.resource.service;
-
-import com.sampleAuth.resource.models.User;
-
-public interface RegistrationService {
-    Boolean register(User user);
-    Boolean update(User user);
-    Boolean delete(String userName);
-}
